f138a014e07761c64ede95cd23d581231609a12b
Oleg <keeperoleg26@gmail.com>
2020-01-29 19:31:43 +0200

    Routes

--- src/client/app.module.ts
+++ src/client/app.module.ts
@@ -1,35 +1,14 @@
 import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
-import { RouterModule, Routes } from 'nest-router'
 import { UserModule } from './user/user.module'
 import { LoggerMiddleware } from './logger';
-import { from } from 'rxjs';
-
-const routes: Routes = [
-  {
-    path: '/api',
-    children: [
-      {
-        path: '/v1',
-        children: [
-          UserModule
-        ]
-      }
-    ],
-  },
-];
-
+import { RoutesModule } from './routers'
 
 @Module({
   imports: [
-    RouterModule.forRoutes(routes),
+    RoutesModule,
     UserModule],
   controllers: [],
   providers: [],
 })
 export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LoggerMiddleware)
-      .forRoutes({ path: 'api/v1/user', method: RequestMethod.ALL });
-  }
 }
---
+++ src/client/routers.ts
@@ -0,0 +1,34 @@
+import { RouterModule, Routes } from 'nest-router'
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { LoggerMiddleware } from './logger';
+
+import { UserModule } from './user/user.module'
+
+const Routers: Routes = [
+  {
+    path: '/api',
+    children: [
+      {
+        path: '/v1',
+        children: [
+          UserModule
+        ]
+      }
+    ],
+  },
+];
+
+@Module({
+  imports: [
+    RouterModule.forRoutes(Routers),
+    UserModule],
+  controllers: [],
+  providers: [],
+})
+export class RoutesModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes({ path: 'api/v1/user', method: RequestMethod.ALL });
+  }
+}
