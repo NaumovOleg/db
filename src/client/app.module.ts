a5ce358f2a9cc245ef90379c2e66545ce7f584d2
Oleg <keeperoleg26@gmail.com>
2020-01-29 19:26:19 +0200

    Added router

--- src/client/app.module.ts
+++ src/client/app.module.ts
@@ -1,9 +1,28 @@
 import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { RouterModule, Routes } from 'nest-router'
 import { UserModule } from './user/user.module'
 import { LoggerMiddleware } from './logger';
+import { from } from 'rxjs';
+
+const routes: Routes = [
+  {
+    path: '/api',
+    children: [
+      {
+        path: '/v1',
+        children: [
+          UserModule
+        ]
+      }
+    ],
+  },
+];
+
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    RouterModule.forRoutes(routes),
+    UserModule],
   controllers: [],
   providers: [],
 })
@@ -11,6 +30,6 @@ export class AppModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(LoggerMiddleware)
-      .forRoutes({ path: 'user', method: RequestMethod.ALL });
+      .forRoutes({ path: 'api/v1/user', method: RequestMethod.ALL });
   }
 }
